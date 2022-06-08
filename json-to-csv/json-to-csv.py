040130254fec153cb757ec5fb99845f997ef4b8b
mrp <mrp@mrp.com>
2022-06-08 14:12:30 -0700

    convert json to csv

--- json-to-csv/json-to-csv.py
+++ json-to-csv/json-to-csv.py
@@ -2,14 +2,48 @@
 
 import json
 from pprint import pprint as print
+import csv
+import glob
 
 
-json_file = "data/file-28.json"
+def main():
+    file_list = glob.glob("json-to-csv/data/*")
+    final_result = []
 
-with open(json_file, 'r') as jf:
-    json_data = json.load(fp=jf)
+    for json_file in file_list:
 
-for data in json_data['included']:
-    data_keys = data.keys()
-    if 'attributes' in data_keys:
-        print(data['attributes'])
+        with open(json_file, 'r', encoding='utf-8') as jf:
+            json_data = json.load(fp=jf)                
+
+        for data in json_data['included']:
+            result = {}
+            data_keys = data.keys()
+
+            if 'attributes' in data_keys:
+                attribute_keys = data['attributes'].keys()
+                required_attrs = ['first-name', 'last-name', 'company-title', 'company-name', 'website']
+
+                for attr in required_attrs:
+
+                    if attr in attribute_keys:
+                        result[attr] = data['attributes'][attr]
+                    # else:
+                    #     result[attr] = 'NONE'
+
+                if result:
+                    final_result.append(result)
+
+    print(final_result)
+
+    csv_file = "json-to-csv/csvfile.csv"
+    try:
+        with open(csv_file, 'w') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=['first-name', 'last-name', 'company-title', 'company-name', 'website'])
+            writer.writeheader()
+            for data in final_result:
+                writer.writerow(data)
+    except IOError:
+        print("I/O error")
+
+if __name__ == '__main__':
+    main()
